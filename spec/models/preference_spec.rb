0bc2dbe0c1950bfc77e495d25cae435bcfebde48
larryvsdata <larryvsdata@gmail.com>
2018-12-11 08:04:06 -0800

    Done.

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Preference, type: :model do
   it "has the right fields" do
     p = Preference.new(artist_sort_order: "DESC", song_sort_order: "ASC", allow_create_artists: false, allow_create_songs: true)
+  #  binding.pry
     expect(p.artist_sort_order.class).to eq String
     expect(p.song_sort_order.class).to eq String
     expect(p.allow_create_artists).to eq false
